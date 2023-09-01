4a97b42c4235d4c75af25d6c00efe87c20c7a445
dongdahai <dongdahai@leadong.com>
2023-09-01 15:31:34 +0800

    first commit

---
+++ src/main/java/com/seayaa/demo/graphql/DemoGraphqlApplication.java
@@ -0,0 +1,14 @@
+package com.seayaa.demo.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class DemoGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGraphqlApplication.class, args);
+    }
+
+}
